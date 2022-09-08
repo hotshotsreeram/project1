b81f7ee555d0b703c32c74c6fe84794435795e70
hotshotsreeram <hotshotsreeram@gmail.com>
2022-09-08 16:36:49 -0400

    2nd commit with variables and values

--- variables.tf
+++ variables.tf
@@ -1,111 +1,76 @@
-provider "aws" {
-  region  = var.aws_region
+variable "aws_region" {
+  type        = string
+  description = "aws region for ec2 insatnce "
 }
 
-resource "aws_vpc" "project_vpc" {
-  cidr_block = var.custom_vpc
-
-  tags = {
-    Name = var.vpc_tags
-  }
+variable "aws_zone" {
+  type        = string
+  description = "aws zone for ec2 instance"
 }
 
-resource "aws_subnet" "project_public_subnet" {
-  vpc_id            = aws_vpc.project_vpc.id
-  cidr_block        = var.public_subnet
-  availability_zone = var.aws_zone
-
-  tags = {
-    Name = var.public_subnet_tags
-  }
+variable "ami" {
+  type        = string
+  description = "ami id for ec2 instance"
 }
 
-resource "aws_subnet" "project_private_subnet" {
-  vpc_id            = aws_vpc.project_vpc.id
-  cidr_block        = var.private_subnet
-  availability_zone = var.aws_zone
-
-  tags = {
-    Name = var.private_subnet_tags
-  }
+variable "type" {
+  type        = string
+  description = "ami instance type"
+  sensitive   = true
 }
 
-resource "aws_internet_gateway" "project_ig" {
-  vpc_id = aws_vpc.project_vpc.id
-
-  tags = {
-    Name = var.internet_gateway_tags
-  }
+variable "vm_tags" {
+  type        = string
+  description = "tags for ec2 instance"
 }
 
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.project_vpc.id
-
-  route {
-    cidr_block = var.route_table
-    gateway_id = aws_internet_gateway.project_ig.id
-  }
-
-  route {
-    ipv6_cidr_block = "::/0"
-    gateway_id      = aws_internet_gateway.project_ig.id
-  }
-
-  tags = {
-    Name = var.route_table_tags
-  }
+variable "vpc_tags"{
+	type= string
+	description= "tags for vpc"
 }
 
-resource "aws_route_table_association" "public_1_rt_a" {
-  subnet_id      = aws_subnet.project_public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
+variable "public_subnet_tags"{
+	type= string
+	description="tags for public subnet"
 }
 
-resource "aws_security_group" "web_sg" {
-  name   = "HTTP and SSH"
-  vpc_id = aws_vpc.project_vpc.id
+variable "private_subnet_tags"{
+	type= string
+	description= "tags for private subnet"
+} 
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = [var.route_table]
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.route_table]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = [var.route_table]
-  }
+variable "internet_gateway_tags"{
+	type= string
+	description= "tags for internet gateway"
 }
 
-resource "aws_instance" "web_instance" {
-  ami           = var.ami
-  instance_type = var.type
-  key_name      = var.key
+variable "route_table_tags"{
+	type= string
+	description= "tags for route table"
+}
 
-  subnet_id                   = aws_subnet.project_public_subnet.id
-  vpc_security_group_ids      = [aws_security_group.web_sg.id]
-  associate_public_ip_address = true
+variable "key" {
+  type        = string
+  description = "key for ec2 instance"
+  sensitive   = true
+}
 
-  user_data = <<-EOF
-  #!/bin/bash -ex
-  
-  sudo apt-get update
-  sudo apt install nginx -y
-  echo "<h1>$(curl https://api.prod.rest/?format=text)</h1>" >  /usr/share/nginx/html/index.html 
-  systemctl enable nginx
-  systemctl start nginx
-  EOF
+variable "public_subnet" {
+  type        = string
+  description = "public subnet ip cidr block"
+}
 
-  tags = {
-    "Name" : var.vm_tags
-  }
+variable "private_subnet" {
+  type        = string
+  description = "private subnet ip cidr block"
+}
+
+variable "route_table" {
+  type        = string
+  description = "route table ip cidr block"
+}
+
+variable "custom_vpc" {
+  type        = string
+  description = "custom vpc ip cidr block"
 }
